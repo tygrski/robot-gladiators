985890c27ae63a4c90fbb6f28786fcd7d42eb444
tygrski <77369211+tygrski@users.noreply.github.com>
2021-08-15 21:01:17 -0500

    first commit

---
+++ assets/js/game.js
@@ -0,0 +1,12 @@
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+console.log("This logs a string, good for leaving your self a message");
+// math
+console.log(10 + 10);
+// what is this
+console.log("Our robots name is " + playerName);
+// creates a function named fight
+function fight()  {
+    window.alert("The fight has begun!");
+}
+// fight();
